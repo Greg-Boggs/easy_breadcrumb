82d8399d21142df6ae564b911f2464e0325c02b0
rogerjose81 <rogerjose81@gmail.com>
2012-03-09 15:14:41 -0500

    Corrected Line Endings

--- tpl/easy_breadcrumb.tpl.php
+++ tpl/easy_breadcrumb.tpl.php
@@ -1,1 +1,28 @@
-<?php/** * @file * Tpl for breadcrumb */?><div class="easy-breadcrumb">  <?php  $n = count($breadcrumb);  $s = $n - 1;  for ($i = 0; $i < $n; ++$i):    ?>    <span class="easy-breadcrumb_segment-wrapper">      <?php echo $breadcrumb[$i]; ?>    </span>    <?php if ($i < $s): ?>      <span class="easy-breadcrumb_segment-separator">        <?php echo $separator; ?>      </span>    <?php endif; ?>  <?php endfor ?></div>
+<?php
+/**
+ * @file
+ * Tpl for breadcrumb
+ */
+?>
+<div class="easy-breadcrumb">
+
+  <?php
+  $n = count($breadcrumb);
+  $s = $n - 1;
+
+  for ($i = 0; $i < $n; ++$i):
+    ?>
+
+    <span class="easy-breadcrumb_segment-wrapper">
+      <?php echo $breadcrumb[$i]; ?>
+    </span>
+
+    <?php if ($i < $s): ?>
+      <span class="easy-breadcrumb_segment-separator">
+        <?php echo $separator; ?>
+      </span>
+    <?php endif; ?>
+
+  <?php endfor ?>
+
+</div>

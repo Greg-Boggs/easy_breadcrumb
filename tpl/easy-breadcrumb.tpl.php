14387e1a1d7166458ee1f02a18e4fa20129700ce
tatarbj <tatarbj@649590.no-reply.drupal.org>
2017-06-20 15:09:40 +0200

    Issue #2666010 by ghuygens, tatarbj: Checkbox to keep the segments separator at the end

--- tpl/easy-breadcrumb.tpl.php
+++ tpl/easy-breadcrumb.tpl.php
@@ -2,7 +2,7 @@
   <div itemscope class="easy-breadcrumb" itemtype="<?php print $list_type; ?>">
     <?php foreach ($breadcrumb as $i => $item): ?>
       <?php print $item; ?>
-      <?php if ($i < $segments_quantity - 1): ?>
+      <?php if ($i < $segments_quantity - $separator_ending): ?>
          <span class="easy-breadcrumb_segment-separator"><?php print $separator; ?></span>
       <?php endif; ?>
     <?php endforeach; ?>
